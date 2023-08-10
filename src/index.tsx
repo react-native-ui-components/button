69366a0bf50a63067af74f9dcb646f327fa63e6f
Morgan Barrett <morganbarrett@icloud.com>
2023-08-10 13:13:26 +0100

    chore: lint

--- src/index.tsx
+++ src/index.tsx
@@ -38,8 +38,8 @@ export const Button = ({
 		]}
 	>
 		<Label
-      title={title}
-      bold={preferred}
+			title={title}
+			bold={preferred}
 			style={{
 				color: color ? color : disabled ? "#999999" : "blue"
 			}}
